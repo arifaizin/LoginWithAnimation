e420b74865c18093fa05d557401b4e27d2046da0
Arif <faizin.arief@gmail.com>
2023-06-26 20:46:20 +0700

    remove function

--- app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/customview/PasswordEditText.kt
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/customview/PasswordEditText.kt
@@ -10,31 +10,36 @@ class PasswordEditText : TextInputEditText {
     constructor(context: Context) : super(context) {
         init()
     }
+
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         init()
     }
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
         init()
     }
 
-    private fun init(){
+    private fun init() {
         addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
                 // Do nothing.
             }
+
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                if (s.toString().length < 8) showError() else hideError()
+                if (s.toString().length < 8) {
+                    setError("Password tidak boleh kurang dari 8 karakter", null)
+                } else {
+                    error = null
+                }
             }
+
             override fun afterTextChanged(s: Editable) {
                 // Do nothing.
             }
         })
-    }
-
-    private fun showError() {
-        setError("Password tidak boleh kurang dari 8 karakter", null)
-    }
-    private fun hideError() {
-        error = null
     }
 }
