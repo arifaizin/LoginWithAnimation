fda76f5739cfb58e12f931cbd8f0c674415ebbc9
Arif <faizin.arief@gmail.com>
2023-06-26 11:11:23 +0700

    implement camera & gallery

--- app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/add/AddStoryActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/add/AddStoryActivity.kt
@@ -1,12 +1,161 @@
 package com.dicoding.picodiploma.loginwithanimation.view.add
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.dicoding.picodiploma.loginwithanimation.R
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import com.dicoding.picodiploma.loginwithanimation.databinding.ActivityAddStoryBinding
+import com.dicoding.picodiploma.loginwithanimation.util.createCustomTempFile
+import com.dicoding.picodiploma.loginwithanimation.util.uriToFile
+import java.io.File
 
 class AddStoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddStoryBinding
+    private lateinit var currentPhotoPath: String
+
+    private var getFile: File? = null
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_story)
+        binding = ActivityAddStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding.cameraButton.setOnClickListener { startTakePhoto() }
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.uploadButton.setOnClickListener { uploadImage() }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this,
+                packageName,
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            myFile.let { file ->
+//              Silakan gunakan kode ini jika mengalami perubahan rotasi
+//              rotateFile(file)
+                getFile = file
+                binding.previewImageView.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this)
+                getFile = myFile
+                binding.previewImageView.setImageURI(uri)
+            }
+        }
+    }
+
+    private fun uploadImage() {
+//        if (getFile != null) {
+//            val file = reduceFileImage(getFile as File)
+//
+//            val description = "Ini adalah deksripsi gambar".toRequestBody("text/plain".toMediaType())
+//            val requestImageFile = file.asRequestBody("image/jpeg".toMediaType())
+//            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
+//                "photo",
+//                file.name,
+//                requestImageFile
+//            )
+//
+//            val apiService = ApiConfig().getApiService()
+//            val uploadImageRequest = apiService.uploadImage(imageMultipart, description)
+//
+//            uploadImageRequest.enqueue(object : Callback<FileUploadResponse> {
+//                override fun onResponse(
+//                    call: Call<FileUploadResponse>,
+//                    response: Response<FileUploadResponse>
+//                ) {
+//                    if (response.isSuccessful) {
+//                        val responseBody = response.body()
+//                        if (responseBody != null && !responseBody.error) {
+//                            Toast.makeText(this@MainActivity, responseBody.message, Toast.LENGTH_SHORT).show()
+//                        }
+//                    } else {
+//                        Toast.makeText(this@MainActivity, response.message(), Toast.LENGTH_SHORT).show()
+//                    }
+//                }
+//                override fun onFailure(call: Call<FileUploadResponse>, t: Throwable) {
+//                    Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
+//                }
+//            })
+//        } else {
+//            Toast.makeText(this@MainActivity, "Silakan masukkan berkas gambar terlebih dahulu.", Toast.LENGTH_SHORT).show()
+//        }
     }
 }
